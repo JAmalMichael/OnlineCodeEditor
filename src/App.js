a77738b7551cafa708d2e5087d759d7f410a7557
John24Woo <eshimi090@gmail.com>
2023-08-21 14:10:03 -1100

    Initialising the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Html from './components/Html';
+import Css from './components/Css';
+import Javascript from './components/Javascript';
 
 function App() {
+const [html,setHtml] = useState('')
+const [css,setCss] = useState('')
+const [js,setJs] = useState('')
+
  return (
   <div>
-      Hello World
+      <div className='section top-section'>
+        <Html 
+        language = 'xml' 
+        displayName="HTML" 
+        value={html} 
+        onChange={setHtml} 
+        />
+        <Css 
+         language = 'css' 
+         displayName="CSS" 
+         value={css} 
+         onChange={setCss} 
+        />
+        <Javascript 
+         language = 'javascript' 
+         displayName="JAVASCRIPT" 
+         value={js} 
+         onChange={setJs} 
+        />
+      </div>
+      <div className='section'>
+        <iframe
+         title='output'
+         sandbox='allow script'
+         frameBorder="0"
+         width="100%"
+         height="100%"
+        />
+      </div>
+
   </div>
  )
 }
