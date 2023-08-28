da8c43dc0dd5a7caf00d65eede0bca5118d9dcfa
John24Woo <eshimi090@gmail.com>
2023-08-28 00:08:13 -1100

    Updating local storage

--- src/hooks/uselocalstorage.js
+++ src/hooks/uselocalstorage.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react'
 
 const PREFIX  = 'codepen-clone-'
 
-export default function uselocalstorage(key, initialValue) {
+export default function useLocalStorage(key, initialValue) {
 
   const prefixedKey = PREFIX + key
 
